79a2902bd4a498e29ad3182e747817d292ff4209
smearedyam <smearedyam@gmail.com>
2020-12-06 11:58:34 -0600

    wipp

---
+++ core/src/com/smearedyam/javagame/world/GameMap.java
@@ -0,0 +1,13 @@
+package com.smearedyam.javagame.world;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public abstract class GameMap {
+	
+	public abstract void render (OrthographicCamera camera);
+	public abstract void update (float delta);
+	public abstract void dispose ();
+	
+	
+	
+}
---
+++ desktop/src/com/smearedyam/javagame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smearedyam.javagame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smearedyam.javagame.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
